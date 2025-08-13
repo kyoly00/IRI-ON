5614d0354923b0758d9d306e10c02f8a56fa837e
Jaemin Sim <20203093@kookmin.ac.kr>
2025-08-13 10:00:24 +0900

    [feat/#5] Add: DB 초기 데이터 주입

---
+++ be/db/seed.py
@@ -0,0 +1,44 @@
+# seed.py
+import csv
+from db.session import SessionLocal
+from models.recipe.recipe import Recipe, Difficulty
+
+difficulty_map = {
+    "초급": Difficulty.EASY,
+    "아무나": Difficulty.ANY,
+    "중급": Difficulty.MEDIUM,
+    "고급": Difficulty.HARD,
+}
+
+def seed():
+    db = SessionLocal()
+
+    with open("data/recipes.csv", newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        if db.query(Recipe).count() == 0:
+            
+            for row in reader:
+                if db.query(Recipe).filter(Recipe.name == row["title"]).first():
+                    continue
+
+                diff_value = difficulty_map.get(row["difficulty"])
+                if not diff_value:
+                    # 매핑 안 되는 값 처리 (예: 기본값 or 스킵)
+                    continue
+                
+                recipe = Recipe(
+                    name=row["title"],
+                    description=row["description"],
+                    image_url=row["main_image"],
+                    time=int(row["time"]) if row["time"].isdigit() else None,
+                    servings=int(row["servings"]) if row["servings"].isdigit() else None,
+                    difficulty=diff_value,
+                    instructions=row["steps"],
+                    tools=row["tools"],
+                    materials=row["materials"],
+                    tips=row["tips"],
+                )
+                db.add(recipe)
+
+                db.commit()
+    db.close()
