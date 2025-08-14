343bf39b4569f5a5632729cf37795608f0cd9751
Jaemin Sim <20203093@kookmin.ac.kr>
2025-08-14 14:50:41 +0900

    [feat/#21] 재료 데이터 초기 db에 저장

--- be/db/seed.py
+++ be/db/seed.py
@@ -2,6 +2,7 @@
 import csv
 from db.session import SessionLocal
 from models.recipe.recipe import Recipe, Difficulty, Category
+from models.domain.ingredient import Ingredient
 
 difficulty_map = {
     "초급": Difficulty.EASY,
@@ -19,13 +20,13 @@ category_map = {
     "기타": Category.OTHER,
 }
 
-def seed():
+def seed_recipes():
     db = SessionLocal()
     try:
         with open("data/recipes.csv", newline="", encoding="utf-8") as f:
             reader = csv.DictReader(f)
             if db.query(Recipe).count() == 0:    
-                existing_names = {r.name for r in db.query(Recipe.name).all()}
+                existing_names = []
 
                 for row in reader:
                     if row["title"] in existing_names:
@@ -60,3 +61,31 @@ def seed():
         raise
     finally:
         db.close()
+
+def seed_ingredients():
+    db = SessionLocal()
+    try:
+        with open("data/recipes_ingredients.csv", newline="", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            if db.query(Ingredient).count() == 0:
+                existing_names = []
+
+                for row in reader:
+                    if row["ingredients"] in existing_names:
+                        continue
+
+                    ingredient = Ingredient(
+                        name=row["ingredients"],
+                    )
+                    db.add(ingredient)
+                    existing_names.append(row["ingredients"])
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
+    finally:
+        db.close()
+
+def seed():
+    seed_recipes()
+    seed_ingredients()
