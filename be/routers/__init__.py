412e81da2a5f45ffb5d06c56e51e8d71b0f73917
Jaemin Sim <20203093@kookmin.ac.kr>
2025-08-19 09:29:55 +0900

    [feat/#21] Fix: 라우터 추가

--- be/routers/__init__.py
+++ be/routers/__init__.py
@@ -2,5 +2,6 @@ from .users import router as users_router
 from .recipes import router as recipes_router
 from .realtime import router as realtime_router
 from .ingredients import router as ingredients_router
+from .tools import router as tools_router
 
-all_routers = [users_router, recipes_router, realtime_router, ingredients_router]
+all_routers = [users_router, recipes_router, realtime_router, ingredients_router, tools_router]
